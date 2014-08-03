94d1abedbbf73a08ceac8a4f9b0288cf38e0572b
xoif <zippoxtc@gmail.com>
2014-08-03 23:21:28 +0200

    funzt

--- src/paetow/seifert/mathquest/EinsActivity.java
+++ src/paetow/seifert/mathquest/EinsActivity.java
@@ -96,7 +96,7 @@ public class EinsActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.eins, menu);
+	//	getMenuInflater().inflate(R.menu.eins, menu);
 		return true;
 	}
 
@@ -105,10 +105,10 @@ public class EinsActivity extends Activity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.menuPunktEins) {
-			return true;
-		}
+	//	int id = item.getItemId();
+		//if (id == R.id.menuPunktEins) {
+		//	return true;
+		//}
 		return super.onOptionsItemSelected(item);
 	}
 	
